74654819b6c8804191f90ff7e03136241dacf1a0
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-14 17:44:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/feb6dd0a1cb8576943c0f.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6299192020').split()]
     # -- FORCE_SUB_CHANNELS = ["BotzPW","AshuSupport","AshutoshGoswami24"] -- # 
-    FORCE_SUB_CHANNELS = int(os.environ.get('FORCE_SUB_CHANNELS', '-1002176367673')).split(',')
+    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'ATXBOTS').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002040299414"))
     PORT = int(os.environ.get("PORT", "8030"))
     
