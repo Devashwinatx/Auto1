4a8ac654441bc8b80f3a95c3d50a30920abb3a33
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-14 17:15:00 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -214,14 +214,14 @@ async def auto_rename_files(client, message):
             del renaming_operations[file_id]
             return await download_msg.edit(e)  
 
-        _bool_metadata = await AshutoshGoswami24.get_metadata(update.message.chat.id)  
+        _bool_metadata = await AshutoshGoswami24.get_metadata(message.chat.id)  
     
         if (_bool_metadata):
             metadata_path = f"Metadata/{new_filename}"
-            metadata = await AshutoshGoswami24.get_metadata_code(update.message.chat.id)
+            metadata = await AshutoshGoswami24.get_metadata_code(message.chat.id)
             if metadata:
 
-                await ms.edit("I Found Your Metadata🔥\n\n__Please Wait...__\n`Adding Metadata ⚡...`")
+                await download_msg.edit("I Found Your Metadata🔥\n\n__Please Wait...__\n`Adding Metadata ⚡...`")
                 cmd = f"""ffmpeg -y -i "{path}" {metadata} "{metadata_path}" """
 
                 process = await asyncio.create_subprocess_shell(
@@ -233,12 +233,12 @@ async def auto_rename_files(client, message):
 
                 try:
                     if er:
-                        return await ms.edit(str(er) + "\n\n**Error**")
+                        return await download_msg.edit(str(er) + "\n\n**Error**")
                 except BaseException:
                     pass
-            await ms.edit("**Metadata Added To The File Successfully ✅**\n\n__**Please Wait...**__\n\n`😈Trying To Downloading`")
+            await download_msg.edit("**Metadata Added To The File Successfully ✅**\n\n__**Please Wait...**__\n\n`😈Trying To Downloading`")
         else:
-            await ms.edit("`😈Trying To Downloading`") 
+            await download_msg.edit("`😈Trying To Downloading`") 
         duration = 0
         try:
             parser = createParser(file_path)
@@ -251,7 +251,7 @@ async def auto_rename_files(client, message):
             
         
         ph_path = None 
-        user_id = int(update.message.chat.id) 
+        user_id = int(message.chat.id) 
         c_caption = await AshutoshGoswami24.get_caption(message.chat.id)
         c_thumb = await AshutoshGoswami24.get_thumbnail(message.chat.id)
 
